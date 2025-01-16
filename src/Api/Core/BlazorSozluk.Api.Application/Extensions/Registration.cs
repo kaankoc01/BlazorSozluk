01d030fe4ede708181083105c28123fa9166dbd2
kaan koc <kaankoc01@gmail.com>
2025-01-16 03:47:35 +0300

    Repository design patttern ve generic repositry

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Extensions/Registration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+
+namespace BlazorSozluk.Api.Application.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddApplicationRegistration(this IServiceCollection services)
+        {
+            var assm = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(assm);
+            services.AddAutoMapper(assm);
+            services.AddValidatorsFromAssembly(assm);
+
+            return services;
+        }
+    }
+}
