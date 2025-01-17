d27c7cad18b0ece59bd815ab7f4eb6463523b2c8
kaan koc <kaankoc01@gmail.com>
2025-01-17 15:58:00 +0300

    user login işlemleri

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/LoginUserCommandHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AutoMapper;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Infrastructure;
+using BlazorSozluk.Common.Infrastructure.Exceptions;
+using BlazorSozluk.Common.Models.Queries;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.User
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand,LoginUserViewModel>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+
+        public LoginUserCommandHandler(IUserRepository userRepository , IMapper mapper , IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var dbUser = await _userRepository.GetSingleAsync(i => i.EmailAddress == request.EmailAddress);
+            if (dbUser == null)
+                throw new DatabaseValidationException("User not Found!");
+
+            var pass = PasswordEnctryptor.Encrpt(request.Password);
+            if (dbUser.Password != pass)
+                throw new DatabaseValidationException("Password is Wrong!");
+            if (!dbUser.EmailConfirmed)
+                throw new DatabaseValidationException("Email Address is not confirmed yet");
+
+            var result = _mapper.Map<LoginUserViewModel>(dbUser);
+
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, dbUser.Id.ToString()),
+                new Claim(ClaimTypes.Email, dbUser.EmailAddress),
+                new Claim(ClaimTypes.Name, dbUser.UserName),
+                new Claim(ClaimTypes.GivenName, dbUser.FirstName),
+                new Claim(ClaimTypes.Surname, dbUser.LastName),
+
+            };
+
+            result.Token = GenerateToken(claims);
+
+            return result;
+
+        }
+
+
+        private string GenerateToken(Claim[] claims)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AuthConfig:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiry = DateTime.Now.AddDays(10);
+
+            var token = new JwtSecurityToken(claims: claims,
+                expires: expiry,
+                signingCredentials: creds,
+                notBefore: DateTime.Now);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/LoginUserCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorSozluk.Common.Models.RequestModels;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.User
+{
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+    {
+        public LoginUserCommandValidator()
+        {
+            RuleFor(i => i.EmailAddress)
+                .NotNull()
+                .EmailAddress(EmailValidationMode.AspNetCoreCompatible)
+                .WithMessage("{PropertyName} not a valid email address");
+
+            RuleFor(i => i.Password)
+                .NotNull()
+                .MinimumLength(6).WithMessage("{PropertyName} should a least be {MinLength} characters");
+        }
+    }
+}
